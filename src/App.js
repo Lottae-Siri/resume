c72ba64bf9d278cd4e27b565f7830df9acb295c6
Korpong <korpong.siribantuengsilp@dtac.co.th>
2019-04-19 20:58:47 +0700

    resume

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './my.css';
+import image from './Kid.jpg'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="App background">
+            <div className="app-left">
+                <img className="image" alt="profile" src={image}></img>
+                <h3>Korpong Siribantuengsilp</h3>
+                <h4>DevOps Engineer</h4>
+            </div>
+            <div className="app-right">
+                <div className="Experience">
+                    <h2 className="title">EXPERIENCE</h2>
+                    <ul>
+                        <li>Implement and maintenance application is running on Linux system.</li>
+                        <li>Develop website for IOT Project.</li>
+                        <li>Analyse problem and figure out solution.</li>
+                    </ul>
+                </div>
+                
+                <hr width="90%" size="5" color="white"></hr>
+                <div className="Education">
+                    <h2 className="title">EDUCATION</h2>
+                    <ul>
+                        <li>Bachelor's degree in Computer Technology at Rajamangala University of Technology Krungtep.</li>
+                    </ul>
+                </div>
+                <hr width="90%" size="5" color="white"></hr>
+                <div className="Hobby">
+                    <h2 className="title">HOBBY</h2>
+                    <ul>
+                        <li>Reading.</li>
+                        <li>Driving, Fishing.</li>
+                        <li>Table Tennis.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
   }
 }
