aada4d8c357dbbbd6e9e2d18b0047a7339c06639
Korpong <korpong.siribantuengsilp@dtac.co.th>
2019-04-20 17:40:08 +0700

    +Added Skills

--- src/App.js
+++ src/App.js
@@ -12,14 +12,30 @@ class App extends Component {
                 <img className="image" alt="profile" src={Profile}></img>
                 <h3>Korpong Siribantuengsilp</h3>
                 <h4>DevOps Engineer</h4>
+                <hr width="90%" size="5" color="white"></hr>
+                <h4>Skills</h4>
+                <div className="Skill">
+                    <ul>
+                        <li>Development: ES6, VueJS, CSS, Shell Script</li>
+                        <li>Database: MySQL, phpMyAdmin</li>
+                        <li>OS: Redhat, CentOS</li>
+                        <li>Network: Switch, Router, Firewall</li>
+                </ul>
+                </div>
+                
             </div>
             <div className="app-right">
                 <div className="Experience">
                     <h2 className="title">EXPERIENCE</h2>
                     <ul>
                         <li>Implement and maintenance application is running on Linux system.</li>
-                        <li>Develop website for IOT Project.</li>
-                        <li>Analyse problem and figure out solution.</li>
+                        <li>Develop front-end website for IOT Project.</li>
+                        <li>Design and develop company website.</li>
+                        <li>Building shell script to operate task by automated.</li>
+                        <li>Analyse problem to figure out solution.</li>
+                        <li>Operate docker, Apache, Tomcat, mySQL Replication.</li>
+                        <li>Implement and maintenance Network and Firewall system.</li>
+                        <li>Operate and maintenance SAN Storage.</li>
                     </ul>
                 </div>
                 
@@ -34,8 +50,8 @@ class App extends Component {
                 <div className="Hobby">
                     <h2 className="title">HOBBY</h2>
                     <ul>
-                        <li>Reading.</li>
-                        <li>Driving, Fishing.</li>
+                        <li>Reading, Watching Movie.</li>
+                        <li>Driving, Biking, Fishing.</li>
                         <li>Table Tennis.</li>
                     </ul>
                 </div>
